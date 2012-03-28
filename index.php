cd13f2d5edc4df2f3447dcb3049b9b15fef44426
Markus Hedlund <znarkus@gmail.com>
2012-03-28 14:15:56 +0200

    display errors

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 
+// Make sure errors are displayed. Change this when the site is public.
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
 // Every URL will be routed here.
 // We should run different controller/actions depending on the URL
 // But this example will always load the "products" controller and 
